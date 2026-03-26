b6cc8028fab46301b7c74c1df031668c902836b1
Tigre-Jaune <abdulsamadalhassan2@gmail.com>
2026-03-26 11:51:58 +0300

    Initial commit: Logo stamping script

---
+++ Logo_Stamp.py
@@ -0,0 +1,36 @@
+import os
+from PIL import Image
+
+
+def add_logo(input, logo_path, output, corner ='bottom-right', padding = 15):
+    
+    # to create an output folder if it doesn't exist
+    os.makedirs(output, exist_ok=True)
+
+    logo = Image.open(logo_path).convert("RGBA")
+    for filename in os.listdir(input):
+        if filename.lower().endswith(('png', 'jpg', 'jpeg')):
+            img_path = os.path.join(input, filename)
+            img = Image.open(img_path).convert("RGBA")
+            
+            img_w, img_h = img.size
+            
+            # Scale logo to any ratio you want of image width (for example here, I use 5%)
+            scale_ratio = (img_w * 0.05) / logo.size[0]
+            logo_w = int(logo.width * scale_ratio)
+            logo_h = int(logo.height * scale_ratio)
+            resized_logo = logo.resize((logo_w, logo_h), Image.Resampling.LANCZOS)
+
+            # Calculate position 
+            if corner == 'bottom-right':
+                position = (img.width - logo_w - padding, img.height - logo_h - padding)
+            else:
+                position = (padding, padding)
+
+            img.paste(resized_logo, position, resized_logo)
+            
+            output_path = os.path.join(output, filename)
+            img.convert("RGB").save(output_path)
+
+# call the function
+add_logo('Logo_Stamp/Photo_folder', 'Logo_Stamp/logo.png', 'Logo_Stamp/Stamped_photos')
